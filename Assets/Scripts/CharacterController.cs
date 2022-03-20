65938e877bbddde3a0761d9adc5f54f7f88ed97f
sariomer96 <46028769+sariomer96@users.noreply.github.com>
2022-03-20 09:28:23 +0300

    Initial commit

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] int speed = 10;
+    [SerializeField] float maxSwerve = 1f; 
+    float lastPosX;
+    float moveX;
+    [SerializeField] float swerveSpeed = 5;
+    [SerializeField] float ClampX;
+
+    void Update()
+    {
+        GetMouseDelta();
+        CharacterMove();
+    }
+ 
+    void GetMouseDelta()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            lastPosX = Input.mousePosition.x;
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+
+            moveX = Input.mousePosition.x - lastPosX;
+
+            lastPosX = Input.mousePosition.x;
+
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            moveX = 0f;
+
+        }
+    }
+    void CharacterMove()
+    {
+    
+        float swerveAmount = moveX * Time.fixedDeltaTime * swerveSpeed;
+
+        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerve, maxSwerve);
+
+        transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward + transform.right * swerveAmount * swerveSpeed, Time.deltaTime * speed);
+       
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -ClampX, ClampX), transform.position.y, transform.position.z);
+    }
+
+}
